bf84192296ad772a10e05e0768eed8b055ee74c5
vamsipamu <venketramana.vamsi@gmail.com>
2025-08-25 12:28:58 +0100

    Create kops.sh

---
+++ kops.sh
@@ -0,0 +1,18 @@
+
+#vim .bashrc
+#export PATH=$PATH:/usr/local/bin/
+#source .bashrc
+
+#! /bin/bash
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+curl -Lo kops https://github.com/kubernetes/kops/releases/download/$(curl -s https://api.github.com/repos/kubernetes/kops/releases/latest | grep tag_name | cut -d '"' -f 4)/kops-linux-amd64
+chmod +x kops
+sudo mv kops /usr/local/bin/kops
+chmod +x kubectl
+mv kubectl /usr/local/bin/kubectl
+
+aws s3api create-bucket --bucket mustafa.project.flm.k8s.local --region us-east-1
+aws s3api put-bucket-versioning --bucket mustafa.project.flm.k8s.local --region us-east-1 --versioning-configuration Status=Enabled
+export KOPS_STATE_STORE=s3://mustafa.project.flm.k8s.local
+kops create cluster --name mustafa.k8s.local --zones us-east-1a,us-east-1b --master-count=1 --master-size t2.large --master-volume-size=30 --node-count=3 --node-size t2.medium --node-volume-size=20
+kops update cluster --name mustafa.k8s.local --yes --admin
